d6e9a3f3a86e66e30c988bb5eb85d957828b1a1b
Adam <datriphopper@gmail.com>
2018-08-25 20:04:42 -0500

    Updated constant names

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -7,10 +7,10 @@ public final class PetContract {
     public static abstract class PetEntry implements BaseColumns {
         public static final String TABLE_NAME = "pets";
         public static final String _ID = BaseColumns._ID;
-        public static final String COLUMN_NAME = "name";
-        public static final String COLUMN_WEIGHT = "weight";
-        public static final String COLUMN_BREED = "breed";
-        public static final String COLUMN_GENDER = "gender";
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
 
         //gender constants
         public static final int GENDER_UNKNOWN = 0;
